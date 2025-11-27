bbf030f0135a31aaf84abfa936cc12a08ee341e4
avite86 <satmyhat@gmail.com>
2025-11-27 21:37:57 +0000

    Initial commit of I-A

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+
+def fun1():
+    print("Hello world!")
+
+def main():
+    fun1()
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
